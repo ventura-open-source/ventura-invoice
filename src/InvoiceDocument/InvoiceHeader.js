87527dbe7bbd4f708e0b9cacd4b48d3ff12142eb
Genesis Guerrero Martinez <genesisdaft@gmail.com>
2018-11-14 12:24:51 +0200

    change cost center

--- src/InvoiceDocument/InvoiceHeader.js
+++ src/InvoiceDocument/InvoiceHeader.js
@@ -5,27 +5,40 @@ import format from 'date-fns/format';
 
 export const currentDate = format(new Date(), 'DD/MMM/YYYY');
 
-const InvoiceHeader = (props) => {
-  const { name, address, location, dniType, dni, invoiceNumber, centerCost, date } = props;
+const InvoiceHeader = props => {
+  const {
+    name,
+    address,
+    location,
+    dniType,
+    dni,
+    invoiceNumber,
+    centerCost,
+    date,
+  } = props;
 
   return (
     <Flex align="flex-end" className="InvoiceHeader" wrap={true}>
-      <Box width={1} style={{ textAlign: 'right'}}>
+      <Box width={1} style={{ textAlign: 'right' }}>
         <p>{name.toUpperCase()}</p>
         <p>{address.toUpperCase()}</p>
         <p>{location.toUpperCase()}</p>
-        <p>{dniType} {dni}</p>
+        <p>
+          {dniType} {dni}
+        </p>
       </Box>
       <Box width={1}>
         <p>ventura TRAVEL GmbH</p>
         <p>Lausitzer Straße 31 in 10999 Berlin</p>
-        <p>Center cost: {centerCost}</p>
-        <p style={{textAlign: 'right'}}>Medellin, { date ? format(date, 'DD/MMM/YYYY') : currentDate}</p>
+        <p>Cost center: {centerCost}</p>
+        <p style={{ textAlign: 'right' }}>
+          Medellin, {date ? format(date, 'DD/MMM/YYYY') : currentDate}
+        </p>
         <p>Invoice Nr: {invoiceNumber}</p>
-        <br/>
+        <br />
         <p>Dear Sr. Kiwitz</p>
         <p>To your request I emit this invoice:</p>
-        <br/>
+        <br />
       </Box>
     </Flex>
   );
